8c5e3428abbbdc98aabe900998db709b87597fbb
AbdunabiA <abdunabiabduvahobov86@gmail.com>
2023-12-05 09:25:21 +0500

    made pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import jsconfigPaths from "vite-jsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
+const manifestForPlugIn = {
+  registerType: "prompt",
+  // includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "BrainBox",
+    short_name: "BrainBox",
+    description: "This is a study platform",
+    icons: [
+      {
+        src: "/192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      // {
+      //   src: "/apple-touch-icon.png",
+      //   sizes: "180x180",
+      //   type: "image/png",
+      //   purpose: "apple touch icon",
+      // },
+      {
+        src: "/512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#105491",
+    background_color: "#105382",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
+  plugins: [react(), jsconfigPaths(), VitePWA(manifestForPlugIn)],
 });
